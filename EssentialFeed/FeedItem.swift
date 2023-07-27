e13dbf364ee90c87114a397698c9a0fe2b428675
Cesar Mejia <rasec8me@gmail.com>
2023-07-27 13:01:39 -0600

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ces Mejia on 27/07/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let decription: String?
+    let location: String?
+    let imageURL: URL
+}
